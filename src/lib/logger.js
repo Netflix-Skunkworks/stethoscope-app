9666ed1b7dd81a0a188321982d7105acc96f1fe4
Rob McVey <mcvey.rob@gmail.com>
2019-02-17 12:15:31 -0800

    Fixed winston default logging format where it does not share an interface with console.log (arbitrary arguments). Added some docs for logging. (#99)
    Updated first launch key so that users will be prompted to add app to 
    startup.

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,3 +1,12 @@
+/**
+ * Global Logger, pipes appropriate logs to file in dev and production incl.
+ * auto-rotation.
+ *
+ *   // visible in dev
+ *   log.info('foo', true, { hello: 'world' })
+ *   // visible in dev and prod
+ *   log.error(new Error("ohnos"))
+ */
 const { app } = require('electron')
 const winston = require('winston')
 const path = require('path')
@@ -5,13 +14,21 @@ require('winston-daily-rotate-file')
 const IS_DEV = process.env.NODE_ENV === 'development'
 
 let log
+// log path is %USERDATA%/Stethoscope/application-log-{YYYY-MM-DD}.log
 let userDataPath = __dirname
-let envPrefix = IS_DEV ? 'dev-' : ''
-let maxFiles = IS_DEV ? '1d' : '3d'
-
 try {
   userDataPath = app.getPath('userData')
 } catch (e) {}
+
+// DEV log file names are prefixed with dev-
+let envPrefix = IS_DEV ? 'dev-' : ''
+let maxFiles = IS_DEV ? '1d' : '3d'
+const logLevels = ['error', 'warn', 'info', 'verbose', 'debug', 'silly']
+// logs that will continue to output in prod
+// change if you want more than 'error' and 'warn'
+const productionLogs = logLevels.slice(0, 2)
+
+
 
 if (!global.log) {
   log = winston.createLogger({
@@ -24,26 +41,37 @@ if (!global.log) {
         zippedArchive: true,
         maxSize: '20m',
         maxFiles
-      }),
-      new winston.transports.Console()
+      })
     ]
   })
 
-  const oldInfo = log.info
-  const oldError = log.error
+  // support multiple arguments to winston logger
+  const wrapper = ( original ) => {
+    return (...args) => original(args.map(o => {
+      if (typeof o === 'string') return o
+      return JSON.stringify(o, null, 2)
+    }).join(' '))
+  }
 
-  log.info = function (...args) {
-    if (IS_DEV || process.env.STETHOSCOPE_DEBUG) {
-      oldInfo.apply(oldInfo, args)
-    }
+  // log all levels in DEV, to default file logger AND console
+  if (IS_DEV) {
+    logLevels.forEach(level =>
+      log[level] = wrapper(log[level])
+    )
+    log.add(new winston.transports.Console({
+      format: winston.format.simple()
+    }));
+  } else {
+    // only error/warn should be logged in prod
+    logLevels.forEach(level => {
+      if (productionLogs.includes(level)) {
+        log[level] = wrapper(log[level])
+      } else {
+        log[level] = () => {}
+      }
+    })
   }
-  log.error = function (...args) {
-    if (args[0] instanceof Error) {
-      oldError(args[0].message, args[0].stack)
-    } else {
-      oldError(...args)
-    }
-  }
+
   global.log = log
 }
 
