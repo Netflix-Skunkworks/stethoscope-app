009b6edcc6987de423e44b9791a2b7c28d0367b3
Rob McVey <rmcvey@netflix.com>
2019-02-17 21:33:49 -0800

    Added documentation, tests for NetworkInterface and findIcon, added React DevTools extension to app (if user has extension installed in Chrome)

--- src/lib/findIcon.js
+++ src/lib/findIcon.js
@@ -1,6 +1,17 @@
 const path = require('path')
 
-module.exports = env => file => {
+const findIcon = env => file => {
   const basePath = env === 'development' ? '../../public' : '../../build'
   return path.join(__dirname, basePath, file)
+}
+
+module.exports = findIcon
+
+if (require.main === module) {
+  const curPath = path.resolve(path.dirname('../../'))
+  console.assert(findIcon('development')('foo') === curPath+'/app/public/foo', 'dev path is not correct')
+  console.log('dev ✓')
+  console.assert(findIcon('production')('foo') === curPath+'/app/build/foo', 'prod path is not correct')
+  console.log('prod ✓')
+  console.log(__filename, 'tests passed')
 }
