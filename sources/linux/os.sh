ac186d71d2e445c34ab5784c3b321393f2daec8d
Erich Smith <erich.smith@lifeomic.com>
2019-02-18 18:28:45 -0500

    OS-related kmd source for linux
    Tested on Alpine, Ubuntu and Centos.

---
+++ sources/linux/os.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env kmd
+exec cat /etc/os-release
+save output
+extract NAME="(.*)"
+save system.platform
+
+load output
+extract VERSION_ID=(.*)
+save system.version
+remove output
+
+exec uname -r
+save system.build
