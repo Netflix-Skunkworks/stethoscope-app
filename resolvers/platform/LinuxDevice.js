33772ae87018ead8364449b07653b05a0d0730bb
Erich Smith <erich.smith@lifeomic.com>
2019-02-23 11:20:49 -0500

    Resolve friendlyName for Linux devices

--- resolvers/platform/LinuxDevice.js
+++ resolvers/platform/LinuxDevice.js
@@ -1,4 +1,7 @@
 module.exports = {
+  async friendlyName (root, args, { kmdResponse }) {
+    return kmdResponse.system.hardwareVersion
+  },
   async disks (root, args, { kmdResponse }) {
     return kmdResponse.disks.volumes
   },
