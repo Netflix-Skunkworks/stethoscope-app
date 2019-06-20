faca6b1eb023a4920c424548c780849e749b1bd6
Rob McVey <rmcvey@netflix.com>
2019-06-19 17:36:59 -0700

    added wait before title parsing

--- src/__tests__/test-build.js
+++ src/__tests__/test-build.js
@@ -25,6 +25,10 @@ const app = new Application({
   path: paths[process.platform]
 })
 
+async function wait(seconds) {
+  return new Promise(resolve => setTimeout(() => resolve(), seconds * 1000))
+}
+
 async function scan (origin) {
   const { stdout } = await exec(`curl -H "Origin: ${origin}" --verbose 'http://127.0.0.1:37370/graphql?query=query%20ValidateDevice(%24policy%3A%20DevicePolicy!)%20%7B%0A%20%20policy%20%7B%0A%20%20%20%20validate(policy%3A%20%24policy)%20%7B%0A%20%20%20%20%20%20status%0A%20%20%20%20%20%20osVersion%0A%20%20%20%20%20%20firewall%0A%20%20%20%20%20%20diskEncryption%0A%20%20%20%20%20%20automaticUpdates%0A%20%20%20%20%20%20screenLock%0A%20%20%20%20%20%20remoteLogin%0A%20%20%20%20%20%20stethoscopeVersion%0A%20%20%20%20%20%20requiredApplications%20%7B%0A%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%20%20status%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%20%20%0A%20%20device%20%7B%0A%20%20%20%20deviceId%0A%20%20%20%20deviceName%0A%20%20%20%20platform%0A%20%20%20%20platformName%0A%20%20%20%20friendlyName%0A%20%20%20%20osVersion%0A%20%20%20%20osName%0A%20%20%20%20osBuild%0A%20%20%20%20firmwareVersion%0A%20%20%20%20hardwareModel%0A%20%20%20%20hardwareSerial%0A%20%20%20%20stethoscopeVersion%0A%20%20%20%20ipAddresses%20%7B%0A%20%20%20%20%20%20interface%0A%20%20%20%20%20%20address%0A%20%20%20%20%20%20mask%0A%20%20%20%20%20%20broadcast%0A%20%20%20%20%7D%0A%20%20%20%20macAddresses%20%7B%0A%20%20%20%20%20%20interface%0A%20%20%20%20%20%20type%0A%20%20%20%20%20%20mac%0A%20%20%20%20%20%20physicalAdapter%0A%20%20%20%20%20%20lastChange%0A%20%20%20%20%7D%0A%20%20%20%20security%20%7B%0A%20%20%20%20%20%20firewall%0A%20%20%20%20%20%20automaticUpdates%0A%20%20%20%20%20%20diskEncryption%0A%20%20%20%20%20%20screenLock%0A%20%20%20%20%20%20remoteLogin%0A%20%20%20%20%20%20automaticAppUpdates%0A%20%20%20%20%20%20automaticSecurityUpdates%0A%20%20%20%20%20%20automaticOsUpdates%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A&variables=%7B%0A%20%20"policy"%3A%20%7B%0A%20%20%20%20"stethoscopeVersion"%3A%20">%3D1.0.0"%2C%0A%20%20%20%20"osVersion"%3A%20%7B%0A%20%20%20%20%20%20"darwin"%3A%20%7B%0A%20%20%20%20%20%20%20%20"ok"%3A%20">%3D10.13.4"%2C%0A%20%20%20%20%20%20%20%20"nudge"%3A%20">%3D10.12.6"%0A%20%20%20%20%20%20%7D%2C%0A%20%20%20%20%20%20"win32"%3A%20%7B%0A%20%20%20%20%20%20%20%20"ok"%3A%20">%3D10.0.16299"%2C%0A%20%20%20%20%20%20%20%20"nudge"%3A%20">%3D10.0.15063"%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%2C%0A%20%20%20%20"firewall"%3A%20"ALWAYS"%2C%0A%20%20%20%20"diskEncryption"%3A%20"ALWAYS"%2C%0A%20%20%20%20"automaticUpdates"%3A%20"SUGGESTED"%2C%0A%20%20%20%20"screenLock"%3A%20"IF_SUPPORTED"%2C%0A%20%20%20%20"remoteLogin"%3A%20"NEVER"%2C%0A%20%20%20%20"requiredApplications"%3A%20%5B%7B%0A%20%20%20%20%20%20"name"%3A"Big-IP"%2C%0A%20%20%20%20%20%20"url"%3A"https%3A%2F%2Fwww.google.com%2Fchrome%2F"%0A%20%20%20%20%7D%5D%0A%20%20%7D%0A%7D&sessionId=034fad3d-9352-f41f-848b-76794010fc25&operationName=ValidateDevice'`)
 
@@ -67,6 +71,8 @@ async function main () {
     const isVisible = await app.browserWindow.isVisible()
     assert.strict.equal(isVisible, true)
     console.log(chalk.green('✓'), 'app is visible')
+
+    await wait(2)
 
     const title = await app.client.getTitle()
     assert.strict.equal(title, `Stethoscope (v${pkg.version})`)
