a164e1005ead23a094cb88cf0d7126151ce850de
Carl-Fredrik Arvidson <carl-fredrik.arvidson@svt.se>
2019-07-08 21:49:43 +0200

    Only quits the app if the main window is closed on linux

--- src/start.js
+++ src/start.js
@@ -27,7 +27,7 @@ import initProtocols from './lib/protocolHandlers'
 import loadReactDevTools from './lib/loadReactDevTools'
 import iconFinder from './lib/findIcon'
 import startGraphQLServer from './server'
-import { IS_MAC, IS_WIN } from './lib/platform'
+import { IS_LINUX, IS_MAC, IS_WIN } from './lib/platform'
 import AutoLauncher from './AutoLauncher'
 import updateInit from './updater'
 
@@ -374,8 +374,8 @@ process.on('uncaughtException', err => {
 })
 
 app.on('window-all-closed', () => {
-  if (!IS_MAC) {
-    app.quit()
+  if (IS_LINUX) {
+    app.quit();
   }
 })
 
