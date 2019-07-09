c46f49dbf1191360b4d627c9374b26eed2a2e39f
Carl-Fredrik Arvidson <carl-fredrik.arvidson@svt.se>
2019-07-09 16:51:38 +0200

    Sets appPath from the context instead of a static string
    This makes it easier to change the app name.

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,14 +1,16 @@
 const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName } = context;
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
 
+  const appName = context.packager.appInfo.productFilename;
+
   return await notarize({
     appBundleId: process.env.APP_BUNDLE_ID,
-    appPath: `dist/mac/Stethoscope.app`,
+    appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
   });
