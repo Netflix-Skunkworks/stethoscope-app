966832551749a07bd271df21379a5a30adcb2eb7
Carl-Fredrik Arvidson <carl-fredrik.arvidson@svt.se>
2019-07-15 23:38:24 +0200

    Override build config and add support for pre-release versions (#154)
    * Override entries in the package.json
    
    * Also template linux path in smoke tests
    
    * Skips the includePrerelease when reading the current version
    
    * Simplifies the stethoscopeVersion

--- src/resolvers/Security.js
+++ src/resolvers/Security.js
@@ -3,6 +3,7 @@ import pkg from '../../package.json'
 import { NUDGE, UNSUPPORTED, ALWAYS, NEVER, INVALID_INSTALL_STATE, INVALID_VERSION, VALID } from '../constants'
 import kmd from '../lib/kmd'
 import { PlatformSecurity } from './platform/'
+import config from '../config'
 
 export default {
   async automaticAppUpdates (root, args, context) {
@@ -70,7 +71,7 @@ export default {
   },
 
   async stethoscopeVersion (root, args, context) {
-    return semver.satisfies(pkg.version, args.stethoscopeVersion)
+    return semver.satisfies(pkg.version, args.stethoscopeVersion, { includePrerelease: config.allowPrerelease })
   },
 
   async screenLock (root, args, context) {
