aeeda5cf4983aff678d33335e04f417c312dce1e
Nathan Cook <nathanc@netflix.com>
2019-07-31 10:12:34 -0700

    Fix defaults

--- src/sources/darwin/com.apple.SoftwareUpdate.sh
+++ src/sources/darwin/com.apple.SoftwareUpdate.sh
@@ -3,27 +3,27 @@ tryExec defaults read /Library/Preferences/com.apple.SoftwareUpdate.plist
 defaultTo
 save output
 extract CriticalUpdateInstall\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.criticalUpdateInstall
 
 load output
 extract AutomaticCheckEnabled\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.automaticCheckEnabled
 
 load output
 extract ConfigDataInstall\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.configDataInstall
 
 load output
 extract AutomaticDownload\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.automaticDownload
 
 load output
 extract AutomaticallyInstallMacOSUpdates\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.automaticallyInstallMacOSUpdates
 
 remove output
--- src/sources/darwin/com.apple.commerce.sh
+++ src/sources/darwin/com.apple.commerce.sh
@@ -3,12 +3,12 @@ tryExec defaults read /Library/Preferences/com.apple.commerce.plist
 defaultTo
 save line
 extract AutoUpdateRestartRequired\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.restartRequired
 
 load line
 extract AutoUpdate\s+=\s+([\d]+);
-defaultTo 0
+defaultTo 1
 save updates.autoUpdate
 
 remove line
